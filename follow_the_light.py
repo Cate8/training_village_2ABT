df1ccf4611d2a57890bad36aad7b0405a37d5915
marinraf <marinraf@gmail.com>
2024-08-26 13:46:10 +0200

    add gitignore

--- follow_the_light.py
+++ follow_the_light.py
@@ -3,7 +3,7 @@ import json
 import random
 import sys
 
-from village.app.data import data
+from village.data import data
 from village.classes.task import Task
 from village.pybpodapi.protocol import Bpod
 
@@ -21,7 +21,7 @@ class FollowTheLight(Task):
 
         Follow The Light Task
         -------------------
-        
+
         This task is a simple visual task where the mouse has to poke the center port to start a trial.
         After the center port is poked, one of the two side ports will be illuminated.
         If the mouse licks the correct side port, it receives a reward.
@@ -43,26 +43,29 @@ class FollowTheLight(Task):
     def start(self):
 
         # get parameters from the settings
-        with open('follow_the_light_settings.json') as f:
+        with open("follow_the_light_settings.json") as f:
             self.task_settings = json.load(f)
 
         ## Initiate variables that will be used in the task and won't change
         # Middle port states
         # Turn on light in the middle port, turn off everything else (it should be off already anyway)
         self.ready_to_initiate_output = [
-            (Bpod.OutputChannels.PWM2, self.task_settings["middle_port_light_intensity"]),
+            (
+                Bpod.OutputChannels.PWM2,
+                self.task_settings["middle_port_light_intensity"],
+            ),
             (Bpod.OutputChannels.PWM1, 0),
             (Bpod.OutputChannels.PWM3, 0),
             (Bpod.OutputChannels.BNC1, 0),
-            ('SoftCode', 255),  # sound off
-            ]
+            ("SoftCode", 255),  # sound off
+        ]
 
         # Holding state
         # Change the light of the middle port to the minimum to indicate start of poke
         self.middle_port_output = [(Bpod.OutputChannels.PWM2, 1)]
 
         # Define what happens if the mouse withdraws the head from the middle port too early
-        self.early_withdrawal_state = 'ready_to_initiate'
+        self.early_withdrawal_state = "ready_to_initiate"
 
         # Time the animal has to choose a side port
         self.timer_for_response = self.task_settings["timer_for_response"] / self.speed
@@ -71,20 +74,22 @@ class FollowTheLight(Task):
         # will return the default value of 0.01 seconds
         self.left_valve_opening_time = data.water_calibration.get_valve_time(
             port=1, volume=self.task_settings["reward_amount_ml"]
-            )
+        )
         self.right_valve_opening_time = data.water_calibration.get_valve_time(
             port=3, volume=self.task_settings["reward_amount_ml"]
-            )
+        )
 
         # determine if punishment is needed
         if self.task_settings["punishment"]:
-            self.punish_condition = 'punish_state'
-            self.punishment_timeout = self.task_settings["punishment_timeout"] / self.speed
+            self.punish_condition = "punish_state"
+            self.punishment_timeout = (
+                self.task_settings["punishment_timeout"] / self.speed
+            )
         else:
             # if no timeout, let the mouse choose again
-            self.punish_condition = 'stimulus_state'
+            self.punish_condition = "stimulus_state"
             self.punishment_timeout = 0
-        
+
         # define inter-trial interval. You can change this every trial if you want
         self.iti = self.task_settings["inter_trial_interval"] / self.speed
 
@@ -101,7 +106,7 @@ class FollowTheLight(Task):
         # start the virtual mouse if it is a class VirtualMouse
         if type(self.virtual_mouse) == VirtualMouse:
             print("Engaging virtual mouse...")
-        
+
         # initiate the plotter if it is a class TrialPlotter
         if type(self.plotter) == TrialPlotter:
             print("Engaging plotter...")
@@ -121,9 +126,9 @@ class FollowTheLight(Task):
         # This is coded as a transition in the 'close_door' state.
         if self.current_trial == 0:
             # Close the door
-            self.start_of_trial_transition = 'close_door'
+            self.start_of_trial_transition = "close_door"
         else:
-            self.start_of_trial_transition = 'ready_to_initiate'
+            self.start_of_trial_transition = "ready_to_initiate"
 
         ## Define the conditions for the trial
         # pick a trial type. In this case it is a random choice between "left" and "right"
@@ -135,55 +140,65 @@ class FollowTheLight(Task):
             # engage the virtual mouse
             _thread.start_new_thread(self.virtual_mouse.move_mouse, ())
 
-        ## Middle port states      
+        ## Middle port states
         # Adjust as you want the timer that the mouse has to hold the head in the middle port
         # For example, you could keep increasing this with each correct trial
-        self.middle_port_hold_timer = self.task_settings["middle_port_hold_time"] / self.speed
+        self.middle_port_hold_timer = (
+            self.task_settings["middle_port_hold_time"] / self.speed
+        )
 
         ## Stimulus states
         self.stimulus_state_output = []
         match self.this_trial_type:
             case "left":
-                self.stimulus_state_output.append((Bpod.OutputChannels.PWM1,
-                                                   self.task_settings["side_port_light_intensity"]))
-                self.left_poke_action = 'reward_state'
+                self.stimulus_state_output.append(
+                    (
+                        Bpod.OutputChannels.PWM1,
+                        self.task_settings["side_port_light_intensity"],
+                    )
+                )
+                self.left_poke_action = "reward_state"
                 self.valve_opening_time = self.left_valve_opening_time
                 self.right_poke_action = self.punish_condition
                 self.valve_to_open = 1
             case "right":
-                self.stimulus_state_output.append((Bpod.OutputChannels.PWM3,
-                                                   self.task_settings["side_port_light_intensity"]))
+                self.stimulus_state_output.append(
+                    (
+                        Bpod.OutputChannels.PWM3,
+                        self.task_settings["side_port_light_intensity"],
+                    )
+                )
                 self.left_poke_action = self.punish_condition
-                self.right_poke_action = 'reward_state'
+                self.right_poke_action = "reward_state"
                 self.valve_opening_time = self.right_valve_opening_time
                 self.valve_to_open = 4
-        
+
         # assemble the state machine
         self.assemble_state_machine()
 
     def assemble_state_machine(self):
         # 'start_of_trial' state that sends a TTL pulse to the BNC channel 2
         self.sma.add_state(
-            state_name='start_of_trial',
+            state_name="start_of_trial",
             state_timer=0.01 / self.speed,
             state_change_conditions={Bpod.Events.Tup: self.start_of_trial_transition},
-            output_actions=[(Bpod.OutputChannels.BNC2, 3)]
+            output_actions=[(Bpod.OutputChannels.BNC2, 3)],
         )
 
         self.sma.add_state(
-            state_name='close_door',
+            state_name="close_door",
             state_timer=0,
-            state_change_conditions={Bpod.Events.Tup: 'ready_to_initiate'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 20)]
-            #TODO: change this softcode to a default one
+            state_change_conditions={Bpod.Events.Tup: "ready_to_initiate"},
+            output_actions=[(Bpod.OutputChannels.SoftCode, 20)],
+            # TODO: change this softcode to a default one
         )
 
         # 'ready_to_initiate' state that waits for the poke in the middle port
         self.sma.add_state(
-            state_name='ready_to_initiate',
+            state_name="ready_to_initiate",
             state_timer=0,
-            state_change_conditions={Bpod.Events.Port2In: 'holding_state'},
-            output_actions=self.ready_to_initiate_output
+            state_change_conditions={Bpod.Events.Port2In: "holding_state"},
+            output_actions=self.ready_to_initiate_output,
         )
 
         # 'holding_state' is the second automatic state
@@ -191,65 +206,69 @@ class FollowTheLight(Task):
         # it goes back to the start or to punishment. If timer is reached, it jumps to
         # 'stimulus_state'.
         self.sma.add_state(
-            state_name='holding_state',
+            state_name="holding_state",
             state_timer=self.middle_port_hold_timer,
             state_change_conditions={
                 Bpod.Events.Port2Out: self.early_withdrawal_state,
-                Bpod.Events.Tup: 'stimulus_state',
-                },
-            output_actions=self.middle_port_output
+                Bpod.Events.Tup: "stimulus_state",
+            },
+            output_actions=self.middle_port_output,
         )
 
         self.sma.add_state(
-            state_name='stimulus_state',
+            state_name="stimulus_state",
             state_timer=self.timer_for_response,
             state_change_conditions={
                 Bpod.Events.Port1In: self.left_poke_action,
                 Bpod.Events.Port3In: self.right_poke_action,
-                Bpod.Events.Tup: 'exit'},
-            output_actions=self.stimulus_state_output
+                Bpod.Events.Tup: "exit",
+            },
+            output_actions=self.stimulus_state_output,
         )
 
         self.sma.add_state(
-            state_name='reward_state',
+            state_name="reward_state",
             state_timer=self.valve_opening_time,
-            state_change_conditions={Bpod.Events.Tup: 'iti'},
+            state_change_conditions={Bpod.Events.Tup: "iti"},
             output_actions=[
                 (Bpod.OutputChannels.Valve, self.valve_to_open),
                 # ('BNC1', OptoCondition_onSidePort)
-                ]
+            ],
         )
 
         self.sma.add_state(
-            state_name='punish_state',
+            state_name="punish_state",
             state_timer=self.punishment_timeout,
-            state_change_conditions={Bpod.Events.Tup: 'iti'},
-            output_actions=[]
+            state_change_conditions={Bpod.Events.Tup: "iti"},
+            output_actions=[],
         )
 
         # iti is the time that the mouse has to wait before the next trial
         self.sma.add_state(
-            state_name='iti',
+            state_name="iti",
             state_timer=self.iti,
-            state_change_conditions={Bpod.Events.Tup: 'exit'},
-            output_actions=[]
+            state_change_conditions={Bpod.Events.Tup: "exit"},
+            output_actions=[],
         )
 
     def after_trial(self):
-        visited_states = [self.bpod.session.current_trial.sma.state_names[i] for i in self.bpod.session.current_trial.states]
+        visited_states = [
+            self.bpod.session.current_trial.sma.state_names[i]
+            for i in self.bpod.session.current_trial.states
+        ]
         if "reward_state" in visited_states:
             result = 1
         else:
             result = 0
-    
+
         # update the plotter
         if type(self.plotter) == TrialPlotter:
-            refresh_rate = 5 # every how many trials you want to update the plot
+            refresh_rate = 5  # every how many trials you want to update the plot
             self.plotter.update_plot(result, refresh_rate)
 
     def close(self):
         # Keep the plot open
         if type(self.plotter) == TrialPlotter:
             self.plotter.keep_plotting()
-            print("Close the plot window to finish the task") 
+            print("Close the plot window to finish the task")
         print("Closing the task")
--- virtual_mouse.py
+++ virtual_mouse.py
@@ -1,28 +1,33 @@
-import random
 import time
 
 import numpy as np
 from village.pybpodapi.protocol import Bpod
 
 
-class VirtualMouse():
+class VirtualMouse:
     def __init__(self, my_bpod):
         self.my_bpod = my_bpod
         # self.bpod = Bpod()
         self.trial_limit = 200
         self.trial_number_counter = 0
-        self.performance = .5
-        self.learning_rate = .005
+        self.performance = 0.5
+        self.learning_rate = 0.005
         self.speed = 50
-    
+
     def portX_in(self, port_number):
         # somehow it has to poke out first for this to work
-        self.my_bpod.manual_override(Bpod.ChannelTypes.INPUT, 'Port', channel_number=port_number, value=0)
-        self.my_bpod.manual_override(Bpod.ChannelTypes.INPUT, 'Port', channel_number=port_number, value=1)
+        self.my_bpod.manual_override(
+            Bpod.ChannelTypes.INPUT, "Port", channel_number=port_number, value=0
+        )
+        self.my_bpod.manual_override(
+            Bpod.ChannelTypes.INPUT, "Port", channel_number=port_number, value=1
+        )
 
     def portX_out(self, port_number):
-        self.my_bpod.manual_override(Bpod.ChannelTypes.INPUT, 'Port', channel_number=port_number, value=0)
-    
+        self.my_bpod.manual_override(
+            Bpod.ChannelTypes.INPUT, "Port", channel_number=port_number, value=0
+        )
+
     def read_trial_type(self, trial_type):
         self.trial_type = trial_type
 
@@ -32,7 +37,7 @@ class VirtualMouse():
             # reverse the probability distribution
             probability_distribution.reverse()
         return np.random.choice([1, 3], p=probability_distribution)
-    
+
     def learn(self):
         self.performance += self.learning_rate
         if self.performance > 1:
@@ -40,20 +45,20 @@ class VirtualMouse():
 
     def move_mouse(self):
         self.movements_for_trial(self.trial_type)
-    
+
     def movements_for_trial(self, trial_type):
         print("Virtual Mouse is starting a new trial!")
         self.trial_number_counter += 1
         # wait a bit for the bpod to load the state matrix
-        time.sleep(.05)
+        time.sleep(0.05)
         # poke in center port
         self.portX_in(2)
-        time.sleep(.5 / self.speed)
+        time.sleep(0.5 / self.speed)
         self.portX_out(2)
         # choose a random port to poke
         port_to_poke = self.port_picker(trial_type, self.performance)
         self.portX_in(port_to_poke)
-        time.sleep(.5 / self.speed)
+        time.sleep(0.5 / self.speed)
         self.portX_out(port_to_poke)
         # learn
         if self.performance < 1:
