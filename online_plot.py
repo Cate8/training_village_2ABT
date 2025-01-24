71f6f091b7c1ed7f139150a21b0da62595a9a33a
HernandoMV <hvergarabio@gmail.com>
2025-01-24 14:51:39 +0100

    .

--- online_plot.py
+++ online_plot.py
@@ -13,29 +13,8 @@ from plotting_functions import plot_side_correct_performance
 class Online_Plot(OnlinePlotFigureManager):
     def __init__(self) -> None:
         super().__init__()
-        # self.required_columns = ["TRIAL_START", "trial"]
-        # self.extra_columns = ["correct"]
         self.ax1 = self.fig.add_subplot(1, 2, 1)
         self.ax2 = self.fig.add_subplot(1, 2, 2)
-
-    # def create_multiplot(self, df: pd.DataFrame) -> Figure:
-    #     try:
-    #         self.df = df[self.required_columns].copy()
-    #         for extra_column in self.extra_columns:
-    #             if extra_column in df.columns:
-    #                 self.df[extra_column] = df[extra_column]
-    #                 if extra_column == "correct":
-    #                     # convert True and False to 1 and 0
-    #                     self.df["correct"] = self.df["correct"].apply(
-    #                         lambda x: int(ast.literal_eval(x))
-    #                     )
-    #         self.make_plot()
-    #         self.fig.tight_layout()
-    #     except Exception:
-    #         self.make_error_plot()
-    #         print("Could not create plot")
-
-    #     return self.fig
 
     def update_plot(self, df: pd.DataFrame) -> None:
         try:
