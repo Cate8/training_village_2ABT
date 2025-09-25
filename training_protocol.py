2a938cf70391c7544cbc2368062f89c402bdbea2
Cate8 <caterinabarezzi18@gmail.com>
2025-09-25 14:59:55 +0200

    calibration 3 females

--- training_protocol.py
+++ training_protocol.py
@@ -158,7 +158,7 @@ class TrainingProtocol(Training):
                     self.settings.next_task = "S2"
                     self.settings.minimum_duration = 25 * 60
                     self.settings.maximum_duration = 45 * 60
-                    self.settings.volume = 5
+                    self.settings.volume = 3
                     self.settings.trials_with_same_side = 20
                     self.settings.led_on_time = 300
                     self.settings.drink_delay_time = 2
