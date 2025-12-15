f7475654c3a8d3fc9adab4b8dcc1af0371286aa6
Cate8 <caterinabarezzi18@gmail.com>
2025-12-15 13:47:53 +0100

    opto task

--- softcode_functions.py
+++ softcode_functions.py
@@ -2,6 +2,7 @@ import time
 
 from sound_functions import sound_device, whitenoise_generator
 from village.manager import manager
+from village.devices.camera import cam_box
 
 
 def function1():
@@ -10,5 +11,12 @@ def function1():
     sound_right = whitenoise_generator(0.5, manager.task.ports.sound_gain_right, 0.005)
     sound_device.load(left=sound_left, right=sound_right)
     sound_device.play()
+
+def function2():
+    cam_box.annotation = "ON"
+
+def function3():
+    cam_box.annotation = ""
+    
 
 
