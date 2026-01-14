150ce79727cff311d098611716eb4066828868a4
Cate8 <caterinabarezzi18@gmail.com>
2026-01-14 06:26:58 +0100

    change in change_cycle

--- change_cycle.py
+++ change_cycle.py
@@ -33,6 +33,7 @@ class ChangeCycle(ChangeCycleBase):
             days=self.days,
             safe=self.safe,
             backup_dir=self.backup_dir,
+            remote=True,
             remote_user=self.remote_user,
             remote_host=self.remote_host,
             port=self.port,
